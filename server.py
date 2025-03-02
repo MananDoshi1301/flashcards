15f3b95dd7ef681a2954526a127a864eae85809c
MananDoshi1301 <manandoshi1301@gmail.com>
2025-03-01 23:37:45 -0700

    Fix for production

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+import os
 from create_app import get_app
 
+app = get_app()
+
 if __name__ == "__main__":
-    PORT = 5000
-    server = get_app()
-    server.run(host='0.0.0.0', port=5000)
+    env = os.environ.get("FLASK_ENV", "development")
+    if env == "development":        
+        app.run(host='0.0.0.0', port=5000)
